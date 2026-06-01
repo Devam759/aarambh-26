594341dbd8d3ae6fad13c4b88f8bf309433734f0
Rashi <rashikatiyar616@gmail.com>
2026-06-01 22:11:55 +0530

    photo change

--- constants/team.ts
+++ constants/team.ts
@@ -69,7 +69,7 @@ export const TEAM_DATA = {
     {
       name: "Vedika Agrawal",
       designation: "Organizing Head",
-      photo: "",
+      photo: "/Team Photos/OH/Vedika Agrawal.webp",
       department: "Core Organizing Committee",
       socials: {
         email: "vedikaagrawal@jklu.edu.in"
@@ -78,7 +78,7 @@ export const TEAM_DATA = {
     {
       name: "Aman Pratap Singh",
       designation: "Organizing Head",
-      photo: "",
+      photo: "/Team Photos/OH/Aman Pratap Singh.webp",
       department: "Core Organizing Committee",
       socials: {
         email: "amanpratapsingh@jklu.edu.in"
@@ -87,7 +87,7 @@ export const TEAM_DATA = {
     {
       name: "Vaishnavi Shukla",
       designation: "Organizing Head",
-      photo: "",
+      photo: "/Team Photos/OH/Vaishnavi Shukla.webp",
       department: "Core Organizing Committee",
       socials: {
         email: "vaishnavishukla@jklu.edu.in"
@@ -96,7 +96,7 @@ export const TEAM_DATA = {
     {
       name: "Tanik Gupta",
       designation: "Organizing Head",
-      photo: "",
+      photo: "/Team Photos/OH/Tanik Gupta.webp",
       department: "Core Organizing Committee",
       socials: {
         email: "tanikgupta@jklu.edu.in"
@@ -105,7 +105,7 @@ export const TEAM_DATA = {
     {
       name: "Ambika Dalmia",
       designation: "Organizing Head",
-      photo: "",
+      photo: "/Team Photos/OH/Ambika Dalmia.webp",
       department: "Core Organizing Committee",
       socials: {
         email: "ambikadalmia@jklu.edu.in"
